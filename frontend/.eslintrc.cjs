86bc852a0b385700b06f691a0fd22ad13f74afea
Ori Baram <obrm770@gmail.com>
2024-01-30 15:08:51 +0200

    merge eslintrc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,22 +1,44 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "off",
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true
   },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect" // This overrides '18.2' to always use the detected React version
+    }
+  },
+  plugins: ["react", "react-refresh", "prettier", "react-hooks"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "prettier/prettier": ["error"],
+    "react/jsx-filename-extension": [1, { extensions: [".jsx"] }],
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn",
+    "indent": ["error", "tab"],
+    "linebreak-style": ["error", "windows"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-console": 1,
+    "stylelint/declaration-block-no-ignored-properties": true,
+    "stylelint/declaration-block-no-duplicate-properties": true,
+    "stylelint/no-unknown-animations": true
+  }
 };
