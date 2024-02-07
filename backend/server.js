8870d9463acf9b6fe9c2be87e8fcb21f318bf6a6
Ori Baram <obrm770@gmail.com>
2024-02-07 13:39:28 +0200

    add colors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 
 dotenv.config({ path: './config/config.env' });
 
@@ -9,7 +10,16 @@ app.get('/', (req, res) => res.send('Server running'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
