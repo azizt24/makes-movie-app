6e082554ca851e72421d9efbc1f0cb4d894ca35d
Mohasalyan <mohammed.s.alyan@gmail.com>
2024-02-13 13:15:05 +0200

    logging

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import logger from './config/logger.js';
+import connectDB from './db/db.js';
 
 dotenv.config({ path: './config/config.env' });
 
@@ -8,18 +10,21 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Server running'));
 
+// Connect to MongoDB
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
+  logger.info(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  logger.error(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
