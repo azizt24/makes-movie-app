77e96132c34946a635915c3636769b0f7e679762
Ori Baram <obrm770@gmail.com>
2024-02-13 15:21:29 +0200

    code review

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import colors from 'colors';
+import colors from 'colors'; // CR -no need for colors. you can remove the import and uninstall the colors package
 import logger from './config/logger.js';
 import connectDB from './db/db.js';
 
@@ -18,13 +18,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   logger.info(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold // CR -no need for colors. you can remove the import and uninstall the colors package
   )
 );
 
 // Handle unhandled promise rejections
+// CR - delete the promise because it is not being used
 process.on('unhandledRejection', (err, promise) => {
-  logger.error(`Error: ${err.message}`.red);
+  logger.error(`Error: ${err.message}`.red); // CR -no need for colors. you can remove the import and uninstall the colors package
   // Close server & exit process
   server.close(() => process.exit(1));
 });
