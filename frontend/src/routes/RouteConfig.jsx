edf14571ab2065b7e3d8d635b0049eee1b32a161
Ori Baram <obrm770@gmail.com>
2024-02-13 14:41:01 +0200

    code review

--- frontend/src/routes/RouteConfig.jsx
+++ frontend/src/routes/RouteConfig.jsx
@@ -31,34 +31,45 @@ import { createBrowserRouter} from 'react-router-dom';
   };
 
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element :  <SharedLayout /> , 
-        errorElement : <Error /> , 
-        children : [ 
-            {
-                index : true, 
-                element : <Home />, 
-            },
-            {
-                path : 'movie', 
-                element : <Movies />, 
-            }, 
-            {
-                path : 'movie/:id', 
-                element : <Movie /> 
-            }, 
-            {
-                path : 'settings', 
-                element : <Settings />
-            }, 
-            {
-                path : 'advanced-search', 
-                element : <AdvancedSearch /> 
-            }
-
-        ]
-    }
+  {
+    path: '/',
+    element: <SharedLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      // CR - remove this item
+      {
+        path: 'movie',
+        element: <Movies />,
+      },
+      {
+        path: 'movie/:id',
+        element: <Movie />,
+      },
+      /* CR - add these routes:
+            'movies/:category/page/:page' - element - Movies
+            'tv/:category/page/:page' - element - TvShows
+            'search/:type/:query/page/:page' - element - MovieListSearch
+            'popular-actors/page/:page' - element - PopularActors
+            'movies/actors/:name/page/:page' - element - ActorMovies
+            'ai-generated-movies' - element - AIGeneratedMovies
+            'ai-generated-movies/:categoryName' - element - AIGeneratedMoviesByCategory
+            'watch/:id' - element - Watch
+            'savedmovies' - element - SavedMovies
+            */
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+      {
+        path: 'advanced-search',
+        element: <AdvancedSearch />,
+      },
+    ],
+  },
 ]); 
 export default router; 
  
