a79d6aa1a726c7803acc7c172920a914b3b5fec5
מוני ברוך <monybaruch@gmail.com>
2024-02-14 13:37:37 +0200

    finishing the movies title

--- frontend/src/pages/MoviesPage/MoviesPage.style.jsx
+++ frontend/src/pages/MoviesPage/MoviesPage.style.jsx
@@ -10,12 +10,33 @@ export const TitleContainer = styled.div`
 `;
 
 export const FirstTitleLetter = styled.span`
-  font-size: 25.2px;
+  font-size: 35px;
   color: rgb(130, 216, 216);
   text-transform: capitalize;
-`;
+  position: relative;
+  width: 27px;
 
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 4px;
+    left: -3px;
+    height: 2px;
+    width: 100%;
+    background: rgb(130, 216, 216);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 1px;
+    height: 2px;
+    width: 70%;
+    background: var(--text-white);
+  }
+`;
 export const RestOfTitle = styled.span`
-  font-size: 16.56px;
+  font-size: 23px;
   color: rgb(255, 255, 255);
 `;
