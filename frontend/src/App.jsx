c5728c328e378d88600a775006c5a3af2403c82d
Mohasalyan <mohammed.s.alyan@gmail.com>
2024-02-20 13:57:03 +0200

    updToast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/RouteConfig'; 
 import { Footer } from './components';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div>
       <h1>Movie App</h1>
       <Footer />
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
