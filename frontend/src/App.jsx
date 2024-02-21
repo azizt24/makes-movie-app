f4b6fb2e080be879e40e6fbbaa29f241f74beb4e
Ori Baram <obrm770@gmail.com>
2024-02-21 10:36:03 +0200

    aprove pull request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import router from './routes/RouteConfig';
 import { Footer } from './components';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <div>
-      <h1>Movie App</h1>
+      <ToastContainer />
       <Footer />
       <RouterProvider router={router} />
-      <ToastContainer />
     </div>
   );
 }
