f91640f64a110ec2964bda2fe8464e59218651ac
Mohasalyan <mohammed.s.alyan@gmail.com>
2024-02-21 14:43:49 +0200

    AddErrorBounadary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
-import router from './routes/RouteConfig'; 
+import router from './routes/RouteConfig';
 import { Footer } from './components';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Fallback } from './components/ErrorBoundary/Fallback';
+import logger from '../../backend/config/logger';
 
+const errorHandler = (error, errorInfo) => {
+logger.info('logging' , error , errorInfo);
+};
 function App() {
   return (
     <div>
       <h1>Movie App</h1>
-      <Footer />
-      <RouterProvider router={router} />
+      <ErrorBoundary FallbackComponent={Fallback} onError={errorHandler}>
+        <Footer />
+        <RouterProvider router={router} />
+      </ErrorBoundary>
     </div>
   );
 }
