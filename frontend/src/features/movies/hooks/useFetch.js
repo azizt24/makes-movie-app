0b1c762b4720832a1a64d257b84118721eefdbec
מוני ברוך <monybaruch@gmail.com>
2024-02-28 14:12:08 +0200

    finising and refactoring the useFetch custom hook

--- frontend/src/features/movies/hooks/useFetch.js
+++ frontend/src/features/movies/hooks/useFetch.js
@@ -1,13 +1,17 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { validateUrl, validateQueryKey } from './utils/useFetchValidation';
 
-export const useFetch = url => {
-  const { isPending, data, isError, error } = useQuery({
-    queryKey: ['movies'],
-    queryFn: async () => {
-      const { data } = await axios.get(url);
-      return data;
-    },
+const queryKeyTags = ['movies'];
+
+export const useFetch = (url, queryKey = '') => {
+  validateUrl(url);
+  validateQueryKey(queryKey, queryKeyTags);
+
+  const { isLoading, data, isError, error } = useQuery(queryKey, async () => {
+    const response = await axios.get(url);
+    return response.data;
   });
-  return { isPending, isError, data, error };
+
+  return { isLoading, isError, data, error };
 };
