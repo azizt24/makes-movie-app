177bb29eafe06065c9a27467fc780903432b9e72
azizt24 <aziztouma030@gmail.com>
2024-03-03 00:17:12 +0200

    new-home

--- frontend/src/pages/Home/Home.styles.jsx
+++ frontend/src/pages/Home/Home.styles.jsx
@@ -35,14 +35,15 @@ export const ButtonsContainer = styled.div`
 `;
 
 export const MovieGrid = styled.div`
-display: grid;
+display: flex;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 50px;
   width: 90%;
   margin-top: 20px;
   margin-bottom: 20px;
   background-color: var(--secondary-color);
-
+  justify-content: center;
+    flex-wrap: wrap;
   @media (max-width: 768px) {
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
     gap: 20px;
