abe2a7693f8458a476917a512f21466495ecb711
Ori Baram <obrm770@gmail.com>
2024-03-03 10:24:06 +0200

    navigate to movie

--- frontend/src/components/MovieCard/MovieCard.jsx
+++ frontend/src/components/MovieCard/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   FlipCard,
   FlipCardInner,
@@ -16,12 +17,15 @@ import {
 
 const MovieCard = ({ movie }) => {
   const [isFlipped, setIsFlipped] = useState(false);
-  const { title, poster_path, release_date, vote_average } = movie;
+  const navigate = useNavigate();
+  const { id, title, poster_path, release_date, vote_average } = movie;
   const posterUrl = poster_path
     ? `https://image.tmdb.org/t/p/w500${poster_path}`
     : '/path/to/default/poster.jpg';
   const year = release_date ? new Date(release_date).getFullYear() : 'N/A';
-
+  const navigateToMovie = () => {
+    navigate(`/movies/${id}`);
+  };
   return (
     <FlipCard
       onMouseEnter={() => setIsFlipped(true)}
@@ -39,7 +43,7 @@ const MovieCard = ({ movie }) => {
         <FlipCardBack>
           <PosterB src={posterUrl} alt={title} />
           {/* CR - add an onClick function to navigate to the movie page */}
-          <DetailsButton>Details</DetailsButton>
+          <DetailsButton onClick={navigateToMovie}>Details</DetailsButton>
           <BackTitle>{title}</BackTitle>
         </FlipCardBack>
       </FlipCardInner>
