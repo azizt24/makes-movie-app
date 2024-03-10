fb9f782e4a986d77e638cd5981c766ac1fb40f27
מוני ברוך <monybaruch@gmail.com>
2024-03-10 11:19:30 +0200

    moving the moviesTitle to the layout folder

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './layout/Navbar/Navbar';
 export { default as Footer } from './layout/Footer/Footer';
 export { default as SharedLayout } from './layout/SharedLayout/SharedLayout';
 export { default as Fallback } from './ErrorBoundary/Fallback';
+export { default as MoviesTitle } from './layout/MoviesTitle/MoviesTitle';
--- frontend/src/features/movies/index.js
+++ frontend/src/features/movies/index.js
@@ -1,6 +1,6 @@
+export { default as MoviesTitle } from '../../components/layout/MoviesTitle/MoviesTitle.jsx';
 export { default as MoviesButtons } from './components/Buttons/MoviesButtons.jsx';
+export { default as Pagination } from './components/Pagination/Pagination.jsx';
 export { default as MoviesList } from './components/MoviesList/MoviesList.jsx';
 export { default as MovieCard } from './components/MovieCard/MovieCard.jsx';
 export { default as Carousel } from './components/Carousel/Carousel.jsx';
-export { default as MoviesTitle } from './components/MoviesTitle/MoviesTitle.jsx';
-export { default as Pagination } from './components/Pagination/Pagination.jsx';
