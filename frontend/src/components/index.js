6a3878a68e454aa5249bca903eb116b39d6e1a21
Mohasalyan <mohammed.s.alyan@gmail.com>
2024-03-18 12:37:33 +0200

    Auth

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,4 +3,3 @@ export { default as Footer } from './layout/Footer/Footer';
 export { default as SharedLayout } from './layout/SharedLayout/SharedLayout';
 export { default as Title } from './layout/Title/TItle';
 export { default as Fallback } from './ErrorBoundary/Fallback';
-//export { default as useAuth} from './useAuth/index';
