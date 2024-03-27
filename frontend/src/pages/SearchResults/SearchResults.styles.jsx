51e6a1a81cb1bef6efe3df62f449154794056695
מוני ברוך <monybaruch@gmail.com>
2024-03-27 13:22:49 +0200

    fixing

--- frontend/src/pages/SearchResults/SearchResults.styles.jsx
+++ frontend/src/pages/SearchResults/SearchResults.styles.jsx
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 
 export const MoviesContainer = styled.div`
   display: flex;
-  flex-wrap: wrap
+  flex-wrap: wrap;
   flex-direction: flex;
   align-items: center;
   justify-content: start;
   padding: 20px;
   gap: 20px;
-  height : 100%;
+  height: 100%;
 
   @media (min-width: 768px) {
     flex-direction: row;
@@ -21,18 +21,26 @@ export const MoviesContainer = styled.div`
 `;
 
 export const MovieGrid = styled.div`
-  
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-gap: 20px;
-width: 100%;
-margin-top: 30px;
-padding: 10px;
-height :100%;
-@media (max-width: 768px) {
-  gap: 10px;
-  padding: 3px;
-}
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+  width: 100%;
+  margin-top: 30px;
+  padding: 10px;
+  height: 100%;
+  @media (max-width: 768px) {
+    gap: 10px;
+    padding: 3px;
+  }
+`;
 
-
+export const NoResultsMessage = styled.div`
+  color: red;
+  font-size: 30px;
+  text-align: center;
+  height: 95vh;
+  padding-top: 200px;
+  @media (max-width: 768px) {
+    padding-top: 10px;
+  }
 `;
